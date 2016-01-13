ace5ffcee02f569db9dc1cb51ba52cb1d325d1cb
Mauricio Bustos <m@bustos.org>
2016-01-13 00:30:09 -0800

    updated table definiiton s

--- src/main/scala/org/bustos/who-won/WhoWonTables.scala
+++ src/main/scala/org/bustos/who-won/WhoWonTables.scala
@@ -27,38 +27,19 @@ import scala.slick.driver.MySQLDriver.simple._
 import spray.json._
 
 object WhoWonTables {
-  case class RestStopCounts()
-  case class RiderUpdates()
-  case class RiderRequest(bibNumber: Int)
-  case class RiderEventsRequest(bibNumber: Int)
-  case class RiderDelete(bibNumber: Int)
-  case class RiderUpdateBib(bibNumber: Int, name: String)
-  case class RiderUpdate(bibNumber: Int, latitude: Double, longitude: Double)
-  case class RestStop(name: String, latitude: Double, longitude: Double)
-  case class RiderConfirm(rider: Rider, update: RiderEvent)
-  case class Rider(bibNumber: Int, name: String, registrationDate: DateTime)
-  case class RiderEvent(bibNumber: Int, latitude: Double, longitude: Double, timestamp: DateTime)
-  case class RiderSummary(bibNumber: Int, name: String, stop: String, timestamp: String, timestampObject: DateTime)
-  case class RiderEventList(list: List[RiderSummary])
+  case class Bet(id: Int, playerId: Int, bookId: Int, spread: Float, amount: Float)
+  case class Bracket(id: Int, year: Int, region: String, seed: Int, teamName: String)
+  case class Player(id: Int, firstName: String, lastName: String, nickname: String)
+  case class Result(year: Int,
+                    homeTeamId: Int, homeTeamBookId: Int,
+                    visitingTeamId: Int, visitingTeamBookId: Int,
+                    homeScore: Int, visitingScore: Int,
+                    resultTimeStamp: DateTime)
 
-  val RaceStartHour = 9.0;
-  val RestStops = List(
-    RestStop("Start", 37.850787, -122.258015),
-    RestStop("Moraga", 37.838825, -122.126016),
-    RestStop("Briones", 37.925907, -122.162653),
-    RestStop("Tilden", 37.904802, -122.244842),
-    RestStop("End", 37.850787, -122.258015)
-  )
-  val RideOnRestStops = RestStops.slice(1, RestStops.length)
-  val StartRestStop = RestStops(0)
-
-  val RestStopsByName = RestStops.map({ x => (x.name, x)}).toMap
-
-  val OffCourse = RestStop("Off Course", 0.0, 0.0)
-
-  val riderTable = TableQuery[RiderTable]
-  val riderEventTable = TableQuery[RiderEventTable]
-  val latestEventPerRider = TableQuery[LatestEventPerRider]
+  val betsTable = TableQuery[BetsTable]
+  val bracketsTable = TableQuery[BracketsTable]
+  val playersTable = TableQuery[PlayersTable]
+  val resultsTable = TableQuery[ResultsTable]
 
   implicit def dateTime =
     MappedColumnType.base[DateTime, Timestamp](
@@ -80,43 +61,56 @@ object WhoWonJsonProtocol extends DefaultJsonProtocol {
     }
   }
 
-  implicit val restStop = jsonFormat3(RestStop)
-  implicit val riderUpdate = jsonFormat3(RiderUpdate)
-  implicit val riderFormat = jsonFormat3(Rider)
-  implicit val riderEventFormat = jsonFormat4(RiderEvent)
-  implicit val riderConfirm = jsonFormat2(RiderConfirm)
-  implicit val riderSummary = jsonFormat5(RiderSummary)
-
+  implicit val bet = jsonFormat5(Bet)
+  implicit val bracket = jsonFormat5(Bracket)
+  implicit val player = jsonFormat4(Player)
+  implicit val result = jsonFormat8(Result)
 }
 
-class RiderTable(tag: Tag) extends Table[WhoWonTables.Rider](tag, "rider") {
+class BetsTable(tag: Tag) extends Table[WhoWonTables.Bet](tag, "bets") {
   import WhoWonTables.dateTime
 
-  def bibNumber = column[Int]("bibNumber")
-  def name = column[String]("name")
-  def registrationDate = column[DateTime]("registrationDate")
+  def id = column[Int]("id")
+  def playerId = column[Int]("playerId")
+  def bookdId = column[Int]("bookId")
+  def spread = column[Float]("spread")
+  def amount = column[Float]("amount")
 
-  def * = (bibNumber, name, registrationDate) <> (WhoWonTables.Rider.tupled, WhoWonTables.Rider.unapply)
+  def * = (id, playerId, bookId, spread, amount) <> (WhoWonTables.Bet.tupled, WhoWonTables.Bet.unapply)
 }
 
-class RiderEventTable(tag: Tag) extends Table[WhoWonTables.RiderEvent](tag, "riderEvent") {
-  import WhoWonTables.dateTime
+class BracketsTable(tag: Tag) extends Table[WhoWonTables.Bracket](tag, "brackets") {
 
-  def bibNumber = column[Int]("bibNumber")
-  def latitude = column[Double]("latitude")
-  def longitude = column[Double]("longitude")
-  def timestamp = column[DateTime]("timestamp")
+  def id = column[Int]("id")
+  def year = column[Int]("year")
+  def region = column[String]("region")
+  def seed = column[Int]("seed")
+  def teamName = column[String]("teamName")
 
-  def * = (bibNumber, latitude, longitude, timestamp) <> (WhoWonTables.RiderEvent.tupled, WhoWonTables.RiderEvent.unapply)
+  def * = (id, year, region, seed, teamName) <> (WhoWonTables.Bracket.tupled, WhoWonTables.Bracket.unapply)
 }
 
-class LatestEventPerRider(tag: Tag) extends Table[WhoWonTables.RiderEvent](tag, "latestEventPerRider") {
+class PlayersTable(tag: Tag) extends Table[WhoWonTables.Player](tag, "players") {
+
+  def id = column[Int]("id")
+  def firstName = column[String]("firstName")
+  def lastName = column[String]("lastName")
+  def nickname = column[String]("nickname")
+
+  def * = (id, firstName, lastName, nickname) <> (WhoWonTables.Player.tupled, WhoWonTables.Player.unapply)
+}
+
+class ResultsTable(tag: Tag) extends Table[WhoWonTables.Result](tag, "results") {
   import WhoWonTables.dateTime
 
-  def bibNumber = column[Int]("bibNumber")
-  def latitude = column[Double]("latitude")
-  def longitude = column[Double]("longitude")
-  def timestamp = column[DateTime]("timestamp")
+  def year = column[Int]("year")
+  def homeTeamId = column[Int]("homeTeamId")
+  def homeTeamBookId = column[Int]("homeTeamBookId")
+  def visitingTeamId = column[Int]("visitingTeamId")
+  def visitingTeamBookId = column[Int]("visitingTeamBookId")
+  def homeScore = column[Int]("homeScore")
+  def visitingScore = column[Int]("visitingScore")
+  def resultTimeStamp = column[DateTime]("resultTimeStamp")
 
-  def * = (bibNumber, latitude, longitude, timestamp) <> (WhoWonTables.RiderEvent.tupled, WhoWonTables.RiderEvent.unapply)
+  def * = (year, homeTeamId, homeTeamBookId, visitingTeamId, visitingTeamBookId, homeScore, visitingScore, resultTimeStamp) <> (WhoWonTables.Result.tupled, WhoWonTables.Result.unapply)
 }
