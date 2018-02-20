ee643111fa5cf733952df76b56c4b1c98f436197
Mauricio Bustos <m@bustos.org>
2018-02-20 05:20:51 +0000

    auto deploy

--- src/main/resources/webapp/main.js
+++ src/main/resources/webapp/main.js
@@ -151,8 +151,8 @@ $(document).ready(function() {
                 var height = img.naturalHeight / img.naturalWidth * width;
                 newCanvas.width = width;
                 newCanvas.height = height;
-                newCanvas.getContext('2d').drawImage(img, 0, 0, newCanvas.width, newCanvas.height);
                 newCanvas.getContext('2d').rotate(Math.PI / 2);
+                newCanvas.getContext('2d').drawImage(img, 0, 0, newCanvas.width, newCanvas.height);
                 var scaledImage = new Image();
                 scaledImage.src = newCanvas.toDataURL();
                 scaledImage.id = 'snapImage';
